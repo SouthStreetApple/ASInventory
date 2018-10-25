c5cf150996c7818f3dd3fae2792a0f1ed8ad7cb8
Jameson <jamesonwking@gmail.com>
2018-10-24 21:43:32 -0400

    Added ProductProvider

--- app/src/main/java/com/example/android/asinventory/MainActivity.java
+++ app/src/main/java/com/example/android/asinventory/MainActivity.java
@@ -148,6 +148,7 @@ public class MainActivity extends AppCompatActivity {
         //Now let's try to select some data and display it
         //Let's get all the rows from the DB table
         Cursor cursor = db.rawQuery("SELECT * FROM " + Inventory.TABLE_NAME, null);
+        
         //Try Block, in case this doesn't work we don't crash the application
         try {
             //Display the rows in the lower text box
